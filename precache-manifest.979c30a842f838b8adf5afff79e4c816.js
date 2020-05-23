99caee95dc529221152c3fd6777095bff4d7bb7b
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-05-23 09:53:16 +0200

    Updates

---
+++ precache-manifest.979c30a842f838b8adf5afff79e4c816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8451cb2443659d6b8372fe763fb0e6b",
+    "url": "/recipeat/index.html"
+  },
+  {
+    "revision": "ec6f86ad96191f7115c4",
+    "url": "/recipeat/static/css/main.04d5b158.chunk.css"
+  },
+  {
+    "revision": "46eabb81f1c1e30f89f9",
+    "url": "/recipeat/static/js/2.786fd059.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipeat/static/js/2.786fd059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6f86ad96191f7115c4",
+    "url": "/recipeat/static/js/main.64d998aa.chunk.js"
+  },
+  {
+    "revision": "f1c6a20d1182c1e94ebf",
+    "url": "/recipeat/static/js/runtime-main.ff2f9df7.js"
+  }
+]);
